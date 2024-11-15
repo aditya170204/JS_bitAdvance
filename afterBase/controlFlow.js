32a35d3b9eb81b867dac244fa1c889e62e44dbef
aditya170204 <adityaraj17feb@gmail.com>
2024-11-15 18:05:30 +0530

    add

---
+++ afterBase/controlFlow.js
@@ -0,0 +1,66 @@
+// const temp = 99
+
+// if (temp > 98) {
+//     var action = "medicine"
+//     console.log(`you have fever , take ${action}`)
+// }
+// console.log(`you have fever , take ${action}`) 
+
+// const balance = 999
+// if (balance > 500) console.log("test")
+// console.log("test2")    
+
+// const temp = 99
+
+// if (temp >= 99) {
+//     let action = "medicine"
+//     console.log(`you have fever , take ${action}`)
+// } else {
+//     let action = "medicine"
+//     console.log(`you are all good,no need to take any ${action}`)
+// }
+
+// const amount = 1200
+// if (amount < 500) {
+//     console.log(`you have less that 500`)
+// } else if (amount < 800) {
+//     console.log(`you have less than 800`)
+// } else if (amount < 1000) {
+//     console.log(`you have less than 1000`)
+
+// } else if (amount == 1111) {
+//     console.log(`you have enough money`)
+// } else {
+//     console.log(`you have more money`)
+// }
+
+// ++++++++++++++++++++++++++++++++++ SWITCH CASE ++++++++++++++++++++++++++++++++++++++++
+
+const day = 5
+switch (day) {
+    case 1: console.log("monday")
+
+        break;
+    case 2: console.log("tuesday")
+
+        break;
+    case 3: console.log("wednesday")
+
+        break;
+    case 4: console.log("thursday")
+
+        break;
+    case 5: console.log("friday")
+
+        break;
+    case 6: console.log("saturday")
+
+        break;
+    case 7: console.log("sunday")
+
+        break;
+
+    default:
+        console.log(`yes the end of week`);
+        break;
+}
