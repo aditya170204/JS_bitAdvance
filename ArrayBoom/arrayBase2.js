1e76af4c7e29dbf37f98cad129ffd6230c52192f
aditya170204 <adityaraj17feb@gmail.com>
2024-12-16 18:12:11 +0530

    array codes

---
+++ ArrayBoom/arrayBase2.js
@@ -0,0 +1,75 @@
+// const supHero = ["spidy", "batty", "suppy"];
+// const supHero2 = ["hulky", "monkey"];
+
+// supHero.push(supHero2);
+// console.log(supHero);
+
+// console.log(supHero[3][1]);
+
+// supHero.concat(supHero2);
+// console.log(supHero);
+
+// supHero.concat(supHero2);
+
+// const allSup = supHero.concat(supHero2);
+// console.log(allSup);
+
+//                                                             //SPREAD
+
+// const newHero = [...supHero, ...supHero2];
+// console.log(newHero);
+
+// const anotherArr = [1, 2, 3, [4, 5, 6], 7, [6, 7, [4, 5]]];
+
+// const realAnotherArr = anotherArr.flat(Infinity);
+// console.log(realAnotherArr);
+
+// console.log(Array.isArray("Aditya"));
+
+// console.log(Array.from("Aditya"));
+
+// console.log(Array.from({ name: "Aditya" })); //     //Returns empty array
+
+// let score1 = 100;
+// let score2 = 200;
+// let score3 = 300;
+// let score4 = 400;
+
+// console.log(Array.of(score1, score2, score3, score4));
+
+// const user = {
+//   username: "Aditya",
+//   weight: 78,
+// };
+// function handleWeight(obj) {
+//   console.log(`username is ${obj.username} and weight is ${obj.weight}`);
+// }
+// handleWeight(user);
+
+// const user = {
+//   username: "Cash",
+//   value: 599,
+// };
+// function handleObject(anyobj) {
+//   console.log(
+//     `username is ${anyobj.username} and the value is ${anyobj.value}`
+//   );
+// }
+// handleObject(user);
+
+// handleObject({
+//   username: "Aditya",
+//   value: "very much",
+// });
+
+// const myNewArr = [200, 400, 100, 600];
+// function ReturnSecondVal(getArr) {
+//   return getArr[1];
+// }
+// console.log(ReturnSecondVal(myNewArr));
+
+// const some = [1, 3, 5, 7, 8];
+// function returnThirdVal(getItem) {
+//   return getItem[2];
+// }
+// console.log(returnThirdVal(some));
